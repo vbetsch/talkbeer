df815a3772fadb0da53b64b92f85a53710a28b4a
vbetsch <vic.betsch@gmail.com>
2024-02-23 00:03:07 +0100

    :label: Complete BeerType

--- src/types/Beer.ts
+++ src/types/Beer.ts
@@ -1,6 +1,13 @@
 interface BeerType {
     id: number
     name: string
+    tagline: string
+    first_brewed: string
     description: string
     image_url: string
+    abv: number
+    ibu: number
+    ebc: number
+    ph: number
+    contributed_by: string
 }
