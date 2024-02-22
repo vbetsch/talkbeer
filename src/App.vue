567d617b920ae12bdf817c9f6d86e921bf2cdfab
vbetsch <vic.betsch@gmail.com>
2024-02-22 15:38:27 +0100

    :sparkles: Router

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-import HomePage from "./views/HomePage.vue";
 </script>
 
 <template>
-    <HomePage/>
+    <div>
+        <router-link :to="{ name: 'Home' }">Home</router-link>
+    </div>
+    <router-view/>
 </template>
 
 <style scoped>
