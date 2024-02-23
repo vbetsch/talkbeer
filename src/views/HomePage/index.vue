a47328330f8cd5cb43fa361af34553fe1403ffb3
vbetsch <vic.betsch@gmail.com>
2024-02-23 11:10:59 +0100

    :building_construction: Merge TheBeerList into view

--- src/views/HomePage/index.vue
+++ src/views/HomePage/index.vue
@@ -1,11 +1,33 @@
 <script setup lang="ts">
-import TheBeerList from "./molecules/TheBeerList.vue";
+import AppStateManagement from "../../components/states/molecules/AppStateManagement.vue";
+import {onMounted} from "vue";
+import {useBeerStore} from "../../stores/BeerStore.ts";
+import {storeToRefs} from "pinia";
+import BeerItem from "./molecules/BeerItem.vue";
+
+const store = useBeerStore()
+const {allBeers} = storeToRefs(store)
+
+onMounted(store.setBeersFromData)
 </script>
 
 <template>
-    <TheBeerList/>
+    <AppStateManagement :loading="store.isLoading" :errorMessage="store.errorMessage"/>
+    <div v-if="!store.isLoading && !store.errorMessage" class="beers">
+        <BeerItem :beer="beer" v-for="beer in allBeers"/>
+    </div>
 </template>
 
 <style scoped>
+.beers {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, 10vw);
+    row-gap: 5vh;
+}
 
+@media (max-width: 1100px) {
+    .beers {
+        grid-template-columns: repeat(auto-fill, 25vw);
+    }
+}
 </style>
