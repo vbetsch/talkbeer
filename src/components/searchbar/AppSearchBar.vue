1ae2153735b8870437d5e97ed8eef3325b446ae1
vbetsch <vic.betsch@gmail.com>
2024-02-24 21:31:55 +0100

    :sparkles: Search - v2

--- src/components/searchbar/AppSearchBar.vue
+++ src/components/searchbar/AppSearchBar.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
 import {faMagnifyingGlass} from '@fortawesome/free-solid-svg-icons';
-import {computed, ref} from "vue";
+import {computed, ref, watch} from "vue";
+import {BeerType} from "../../types/Beer.ts";
 
 export interface AppSearchBarProps {
+    list: BeerType[]
     placeholder: string
     totalWidth?: string
     iconWidth?: string
@@ -14,17 +16,34 @@ const props = withDefaults(defineProps<AppSearchBarProps>(), {
     iconWidth: "15px"
 })
 
-defineEmits<{
-    applyFilter: [text: string]
-    disableSearchMode: []
+const emit = defineEmits<{
+    displayOriginalList: [status: boolean]
+    applyFilter: [list: BeerType[]]
 }>()
 
 let _gap = ref<string>("5px")
 let inputValue = ref<string>("")
+let searchMode = ref<boolean>(false)
 
 const computeTotalWidth = computed(() => `width: ${props.totalWidth};`)
 const computeIconSize = computed(() => `width: ${props.iconWidth}; height: ${props.iconWidth};`)
 const computeInputSize = computed(() => `width: calc(calc(100% - ${props.iconWidth}) - ${_gap.value});`)
+
+const getDataFromFilter = (input: string) => {
+    return props.list.filter((beer: BeerType) => (beer.name.toLowerCase().includes(input.toLowerCase())))
+}
+
+watch(inputValue, (newValue, oldValue) => {
+    if (searchMode && newValue !== oldValue) {
+        const data = getDataFromFilter(newValue)
+        emit('applyFilter', data)
+    }
+})
+watch(searchMode, (newValue, oldValue) => {
+    if (newValue !== oldValue) {
+        emit('displayOriginalList', !newValue)
+    }
+})
 </script>
 
 <template>
@@ -35,8 +54,8 @@ const computeInputSize = computed(() => `width: calc(calc(100% - ${props.iconWid
             v-model="inputValue"
             :style="computeInputSize"
             :placeholder="placeholder"
-            @focusout="$emit('disableSearchMode')"
-            @keyup="$emit('applyFilter', inputValue)"
+            @focusout="searchMode = false"
+            @keyup="searchMode = true"
         >
         <div class="icon-container" :style="computeIconSize">
             <FontAwesomeIcon
--- src/views/HomePage/index.vue
+++ src/views/HomePage/index.vue
@@ -6,29 +6,39 @@ import AppSearchBar from "../../components/searchbar/AppSearchBar.vue";
 import {BeerType} from "../../types/Beer.ts";
 
 const store = useBeerStore()
+const textNoFound = ref<string>("")
+let displayOriginalBeers = ref<boolean>(true)
 let displayedBeers = reactive<BeerType[]>(store.allBeers)
-let searchMode = ref<boolean>(false)
 
 onMounted(store.setBeersFromData)
 
-const callbackApplyFilter = (text: string) => {
-    searchMode.value = true;
-    displayedBeers = store.allBeers.filter((beer: BeerType) => (beer.name.includes(text)))
+const callbackApplyFilter = (data: BeerType[]) => {
+    console.log("(24/02/2024 20:48) @vbetsch [ index.vue:14 -  - callbackApplyFilter ] data", data);
+    textNoFound.value = data.length ? "" : "No beer found"
+    displayedBeers = [
+        ...data
+    ]
 }
 
-const callbackDisableSearchMode = () => searchMode.value = false
+const callbackDisplayOriginalList = (state: boolean) => {
+    displayOriginalBeers.value = state
+    console.log( "(24/02/2024 21:23) @vbetsch [ index.vue:24 -  - callbackDisplayOriginalList ] displayOriginalBeers.value", displayOriginalBeers.value );
+}
 </script>
 
 <template>
     <AppSearchBar
         placeholder="Rechercher... (temp)"
         totalWidth="150px"
-        @applyFilter="callbackApplyFilter"
-        @disableSearchMode="callbackDisableSearchMode"/>  <!-- TODO: Move to navbar -->
+        :list="store.allBeers"
+        @displayOriginalList="callbackDisplayOriginalList"
+        @applyFilter="callbackApplyFilter"/>  <!-- TODO: Move to navbar -->
+    <span v-if="!displayOriginalBeers && textNoFound">{{ textNoFound }}</span>
     <BeerList
+        v-else
         :loading="store.isLoading"
         :error="store.errorMessage"
-        :list="searchMode ? displayedBeers : store.allBeers"
+        :list="displayOriginalBeers ? store.allBeers : displayedBeers"
     />
 </template>
 
