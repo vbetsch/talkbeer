53c27c6b1218ed1d77967635a375d44a9cb49f38
vbetsch <vic.betsch@gmail.com>
2024-02-24 19:09:18 +0100

    :lipstick: Rewrite search bar style

--- src/components/searchbar/AppSearchBar.vue
+++ src/components/searchbar/AppSearchBar.vue
@@ -1,46 +1,85 @@
 <script setup lang="ts">
 import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
 import {faMagnifyingGlass} from '@fortawesome/free-solid-svg-icons';
+import {computed, ref} from "vue";
 
 export interface AppSearchBarProps {
     placeholder: string
+    totalWidth?: string
+    iconWidth?: string
 }
 
 const props = defineProps<AppSearchBarProps>()
+
+let _total_width = ref(props.totalWidth ?? "300px")
+let _icon_width = ref(props.iconWidth ?? "15px")
+let inputValue = ref<string>("")
+let searchMode = ref<boolean>(false)
+
+// TODO: Move to navbar
+// function onKeyUpSearchBar() {
+//     searchMode.value = true;
+//     displayedBeers = store.allBeers.filter((beer: BeerType) => (
+//         beer.name.includes(inputValue.value)
+//     ))
+// }
+
+const computeTotalWidth = computed(() => `width: ${_total_width.value};`)
+const computeIconSize = computed(() => `width: ${_icon_width.value}; height: ${_icon_width.value};`)
 </script>
 
 <template>
-    <div class="searchbar">
-        <input class="search-input" type="text" :placeholder="props.placeholder">
-        <FontAwesomeIcon class="icon" :icon="faMagnifyingGlass"/>
+    <div class="searchbar" :style="computeTotalWidth">
+        <input
+            class="search-input"
+            type="text"
+            v-model="inputValue"
+            :placeholder="placeholder"
+            @keyup=""
+            @focusout="searchMode = false"
+        >
+        <div class="icon-container" :style="computeIconSize">
+            <FontAwesomeIcon
+                class="icon"
+                :icon="faMagnifyingGlass"
+            />
+        </div>
     </div>
 </template>
 
 <style scoped>
 .searchbar {
-    position: relative;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border: 1px solid #b7b7b7;
+    border-radius: 15px;
+    padding: 5px 10px;
+    gap: 5%;
 }
 
 .search-input {
-    border-radius: 15px;
-    border: 1px solid #b7b7b7;
+    border: none;
     outline: none;
-    padding: 3px 32px 3px 10px;
+}
+
+.icon-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
 }
 
 .icon {
-    position: absolute;
+    height: 100%;
+    width: 100%;
     color: #b7b7b7;
-    top: 24%;
-    right: 4%;
 }
 
-.search-input:focus {
+.searchbar:has(.search-input:focus) {
     border-color: var(--grey);
 }
 
 .search-input:focus + .icon {
     color: var(--grey);
 }
-
 </style>
