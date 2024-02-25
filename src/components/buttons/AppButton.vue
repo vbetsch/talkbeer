a60cb498a8c21e4f0fe8cdd5e341e47dc51c82f5
vbetsch <vic.betsch@gmail.com>
2024-02-25 14:56:11 +0100

    :sparkles: Like beer

---
+++ src/components/buttons/AppButton.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+import {IconDefinition} from "@fortawesome/free-solid-svg-icons";
+import {ref} from "vue";
+
+export interface ButtonProps {
+    label?: string
+    icon?: IconDefinition
+    iconHover?: IconDefinition
+}
+
+const props = withDefaults(defineProps<ButtonProps>(), {})
+
+let currentIcon = ref<IconDefinition | undefined>(props.icon)
+
+const emit = defineEmits<{
+    doAction: []
+}>()
+
+const onClick = () => {
+    emit('doAction')
+}
+
+const onMouseEnter = () => {
+    if (props.icon && props.iconHover) {
+        currentIcon.value = props.iconHover
+    }
+}
+const onMouseLeave = () => {
+    if (props.icon) {
+        currentIcon.value = props.icon
+    }
+}
+</script>
+
+<template>
+    <div class="button"
+         @click="onClick"
+         @mouseenter="onMouseEnter"
+         @mouseleave="onMouseLeave"
+    >
+        <FontAwesomeIcon v-if="icon" class="icon" :icon="currentIcon"/>
+        <span v-if="label">{{ label }}</span>
+    </div>
+</template>
+
+<style scoped>
+.button {
+    display: flex;
+    height: fit-content;
+    justify-content: center;
+    align-items: center;
+    border-radius: 7px;
+    padding: 5px;
+}
+</style>
--- src/views/BeerDetailsPage/organisms/BeerDetails.vue
+++ src/views/BeerDetailsPage/organisms/BeerDetails.vue
@@ -2,6 +2,10 @@
 import BeerDetailsInfos from "../molecules/BeerDetailsInfos.vue";
 import {BeerType} from "../../../types/Beer.ts";
 import BeerDetailsTitles from "../molecules/BeerDetailsTitles.vue";
+import AppButton from "../../../components/buttons/AppButton.vue";
+import {faHeart as faRegularHeart} from "@fortawesome/free-regular-svg-icons";
+import {faHeart as faSolidHeart} from "@fortawesome/free-solid-svg-icons";
+import {computed, ref} from "vue";
 
 export interface BeerDetailsProps {
     beer: BeerType
@@ -10,6 +14,27 @@ export interface BeerDetailsProps {
 }
 
 const props = defineProps<BeerDetailsProps>()
+let favorites = ref<BeerType[]>(JSON.parse(localStorage.getItem("favorites") || "[]"));
+let color = ref<string>("var(--white)")
+
+const computeButtonStyle = computed(() => `color: ${color.value};`)
+
+const callbackDoAction = () => {
+    if (favorites.value.includes(props.beer)) {
+        const index = favorites.value.indexOf(props.beer, 0);
+        if (index > -1) {
+            favorites.value.splice(index, 1);
+        }
+        color.value = "var(--white)"
+    } else {
+        favorites.value = [
+            ...favorites.value,
+            props.beer
+        ]
+        color.value = "#ff00e3"
+    }
+    localStorage.setItem("favorites", JSON.stringify(favorites.value));
+}
 </script>
 
 <template>
@@ -18,6 +43,12 @@ const props = defineProps<BeerDetailsProps>()
         <div class="more">
             <span>Contributed by <span class="value">{{ beer.contributed_by }}</span></span>
             <span>First Brewed <span class="value">{{ beer.first_brewed }}</span></span>
+            <AppButton
+                class="button"
+                :icon="favorites.includes(beer) ? faSolidHeart : faRegularHeart"
+                :style="computeButtonStyle"
+                @doAction="callbackDoAction"
+            />
         </div>
         <section class="section description">
             <h4 class="title">Description</h4>
@@ -49,6 +80,7 @@ const props = defineProps<BeerDetailsProps>()
 
 .more {
     display: flex;
+    align-items: center;
     gap: 30px;
     color: var(--grey);
 }
@@ -56,6 +88,14 @@ const props = defineProps<BeerDetailsProps>()
 .more .value {
     color: var(--black);
     font-weight: normal;
+}
+
+.more .button {
+    background-color: #230033;
+}
+
+.more .button:hover {
+    color: #ff00e3 !important;
 }
 
 .section {
